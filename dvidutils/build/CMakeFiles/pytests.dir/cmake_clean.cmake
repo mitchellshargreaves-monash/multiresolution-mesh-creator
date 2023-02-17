4979288b4ca67ccd8b50a9b8865e747f9121f19e
Nora <nora.hamacher@gmail.com>
2023-02-17 05:35:34 +0000

    remove build

--- dvidutils/build/CMakeFiles/pytests.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/pytests"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/pytests.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
